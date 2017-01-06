51c945c596bc22ba3968c8cb370b539ebee18112
ivanlonel <ivanlonel91@gmail.com>
2017-01-06 16:49:54 -0200

    .

--- src/rc4.c
+++ src/rc4.c
@@ -8,20 +8,19 @@ static void swap_byte(byte_t *x, byte_t *y) {
 
 void prepare_key(const byte_t *RESTRICT_IF_SUPPORTED key_data_ptr, const size_t key_data_len, rc4_key_t *RESTRICT_IF_SUPPORTED key) {
     size_t counter;
-    byte_t index1 = 0;
-    byte_t index2 = 0;
 
-    key->x = 0;
-    key->y = 0;
+    key->x = key->y = 0; /* Initializing for temporary use inside this function. */
 
     for(counter = 0; counter < SEED_SIZE; counter++)
         key->state[counter] = (byte_t) counter;
 
     for(counter = 0; counter < SEED_SIZE; counter++) {
-        index2 = (byte_t) ((key_data_ptr[index1] + key->state[counter] + index2) % SEED_SIZE);
-        swap_byte(&key->state[counter], &key->state[index2]);
-        index1 = (byte_t) ((index1 + 1u) % key_data_len);
+        key->y = (byte_t) ((key_data_ptr[key->x] + key->state[counter] + key->y) % SEED_SIZE);
+        swap_byte(&key->state[counter], &key->state[key->y]);
+        key->x = (byte_t) ((key->x + 1u) % key_data_len);
     }
+
+    key->x = key->y = 0; /* Initializing for external use. */
 }
 
 void rc4(byte_t *RESTRICT_IF_SUPPORTED buffer_ptr, const size_t buffer_len, rc4_key_t *RESTRICT_IF_SUPPORTED key) {
