91400ce99c5711b27aabe9d1650ce9ebf818b1af
ivanlonel <ivanlonel91@gmail.com>
2017-01-09 19:04:21 -0200

    .

--- make.bat
+++ make.bat
@@ -25,9 +25,11 @@ REM Call 'make' with all the arguments passed to this batch script
 %MK_EXEC% BINNAME=rc4.exe %*
 
 
-::set OUTPUT="/Ox /MD /Febin\rc4Dynamic.exe"
-::set OUTPUT="/Ox /MT /Febin\rc4Static.exe"
-::set OUTPUT="/Z7 /MDd /Febin\rc4DynamicDebug.exe"
-::set OUTPUT="/Z7 /MTd /Febin\rc4StaticDebug.exe"
+::set OUTPUT="/Ox /ML" 	REM /ML is the compiler's default option. Causes the compiler to place the library name LIBC.lib into the .obj file so that the linker will use LIBC.lib to resolve external symbols. LIBC.lib does not provide multithread support.
+::set OUTPUT="/Ox /MT" 	REM /MT Causes the application to use the multithread, static version of the run-time library. Defines _MT and causes the compiler to place the library name LIBCMT.lib into the .obj file so that the linker will use LIBCMT.lib to resolve external symbols.
+::set OUTPUT="/Ox /MD" 	REM /MD Causes the application to use the multithread-specific and DLL-specific version of the run-time library. Defines _MT and _DLL and causes the compiler to place the library name MSVCRT.lib into the .obj file. Applications compiled with this option are statically linked to MSVCRT.lib. This library provides a layer of code that enables the linker to resolve external references. The actual working code is contained in MSVCR<versionnumber>.DLL, which must be available at run time to applications linked with MSVCRT.lib.
+::set OUTPUT="/Z7 /MLd"	REM /MLd Defines _DEBUG and causes the compiler to place the library name LIBCD.lib into the .obj file so that the linker will use LIBCD.lib to resolve external symbols. LIBCD.lib does not provide multithread support.
+::set OUTPUT="/Z7 /MTd"	REM /MTd Defines _DEBUG and _MT. This option also causes the compiler to place the library name LIBCMTD.lib into the .obj file so that the linker will use LIBCMTD.lib to resolve external symbols.
+::set OUTPUT="/Z7 /MDd"	REM /MDd Defines _DEBUG, _MT, and _DLL and causes the application to use the debug multithread-specific and DLL-specific version of the run-time library. It also causes the compiler to place the library name MSVCRTD.lib into the .obj file.
 
-::cl src\*.c /Iinclude %OUTPUT% /Za /MP /Wall /D_CRT_SECURE_NO_WARNINGS /nologo /analyze:stacksize 65536
+::cl src\*.c /Iinclude /Fo.o\ /Febin\rc4.exe /Za %OUTPUT% /MP /nologo /Wall /D_CRT_SECURE_NO_WARNINGS /analyze:stacksize 65536
