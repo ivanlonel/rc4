a52e8d9ebee738a0be3749f51e7e2eb3a059b56a
ivanlonel <ivanlonel91@gmail.com>
2017-01-13 22:23:15 -0200

    .

--- make.bat
+++ make.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Compiler path. May contain the 'make' application and other utilities as well.
-set CC_PATH=C:\mingw-w64\x86_64-6.2.0-release-posix-seh-rt_v5-rev1\mingw64\bin
+::set CC_PATH=C:\mingw-w64\x86_64-6.2.0-release-posix-seh-rt_v5-rev1\mingw64\bin
 ::set CC_PATH=C:\mingw-w64\x86_64-5.4.0-release-posix-seh-rt_v5-rev0\mingw64\bin
 ::set CC_PATH=C:\mingw-w64\x86_64-4.9.3-release-posix-seh-rt_v4-rev1\mingw64\bin
-::set CC_PATH=C:\Dev-Cpp\MinGW64\bin
+set CC_PATH=C:\Dev-Cpp\MinGW64\bin
 ::set CC_PATH=C:\CodeBlocks\MinGW\bin
 ::set CC_PATH=C:\cygwin64\bin
 
@@ -14,15 +14,15 @@ set UTILS_PATH=C:\msys\bin
 ::set UTILS_PATH=C:\unxutils\usr\local\wbin
 
 REM Name of the 'make' application, to be found within the PATH.
-set MK_EXEC=mingw32-make.exe
-::set MK_EXEC=make.exe
+::set MK_EXEC=mingw32-make.exe
+set MK_EXEC=make.exe
 
-REM Appending paths to PATH (instead of prepending) so that Windows keeps using C:\Windows\System32\find.exe instead of GNU 'find' on next runs.
+REM Appending paths to PATH (instead of prepending) so that Windows keeps using %SystemRoot%\System32\find.exe instead of GNU 'find' on next runs.
 echo ;%PATH%; | find /i /c ";%CC_PATH%;" >nul || set PATH=%PATH%;%CC_PATH%
 echo ;%PATH%; | find /i /c ";%UTILS_PATH%;" >nul || set PATH=%PATH%;%UTILS_PATH%
 
-REM Call 'make' with all the arguments passed to this batch script
-%MK_EXEC% BINNAME=rc4.exe %*
+REM Call 'make' with all the arguments passed to this batch script, plus BINNAME with an explicit .exe extension and a minimal PATH to avoid ambiguity with Windows utility names (like 'find')
+%MK_EXEC% BINNAME=rc4.exe PATH="%CC_PATH%;%UTILS_PATH%" %*
 
 
 ::set OUTPUT="/Ox /ML" 	REM /ML is the compiler's default option. Causes the compiler to place the library name LIBC.lib into the .obj file so that the linker will use LIBC.lib to resolve external symbols. LIBC.lib does not provide multithread support.
